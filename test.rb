0a7e0b8d640a6197dffc0779edd45220cb3b659d
Kensei itatani <kensei.itatani@mulodo.com>
2017-11-21 11:35:10 +0700

    add test.rb

---
+++ test.rb
@@ -0,0 +1,27 @@
+utput "I love Ruby"
+say = "I love Ruby"
+puts say
+
+# Output "I *LOVE* RUBY"
+say['love'] = "*love*"
+puts say.upcase
+
+# Output "I *love* Ruby"
+# five times
+5.times { puts say }
+
+class Numeric 
+  def plus (x)
+    self.+(x)
+  end
+end
+
+def hi(name = world)
+  puts "Hello #{name.capitalize}!"
+end
+
+hi "Matz"
+
+hi "Kensei"
+
+hi "My name is kensei"
